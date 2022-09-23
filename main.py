41ca1b6630bb15e6be359b7f64d4598f6a5ea670
Nick Viscomi <nicholasviscomi@gmail.com>
2022-09-23 13:59:16 -0400

    fixed it to use dot products

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+from typing import List
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("data/train.csv")
-data = np.array(data)
 # Structure of data:
 # first column is the label of what the number actually is
 # All the other columns are the values for each pixel, either 0 or 1
@@ -35,35 +34,21 @@ b2 = [] # second biases = 10 for output nodes
 def init_weights_biases():
     w1 = np.random.rand(16, 784) - 0.5 # 16 arrays with 748 random numbers from -0.5 to 0.5
     b1 = np.random.rand(16, 1) - 0.5
-    w2 = np.random.rand(10, 16)
-    b2 = np.random.rand(10, 1)
+    w2 = np.random.rand(16, 10) - 0.5
+    b2 = np.random.rand(1, 10) - 0.5
     return w1, b1, w2, b2
 
 def tanh(x):
     return np.tanh(x)
 
-def feed_forward(inputs, w1, b1, w2, b2):
+def deriv_tanh(x):
+    return 1 / (np.cosh(x) ** 2)
+
+def feed_forward(inputs, w1, b1, w2, b2) -> List:
     # run the inputs through the network
-    h1 = []
-    for i in range(16): # i = hidden layer node index
-        # val = [inputs[j] + w1[i][j] for j in range(w1[i])] + b1[i]
-        # val = (inputs @ w1[i]) + b1[i]
-        total = 0
-        for j in range(784):
-            total += inputs[j] * w1[i][j]
-
-        h1.append(tanh(total + b1[i])) # activation of the total with bias
-
-    o1 = []
-    for i in range(10):
-        # val = (h1[j] + w2[i][j] for j in range(w2[i])) + b2[i]
-        # val = (h1 @ w2[i]) + b2[i]
-        total = 0
-        for j in range(16):
-            total += h1[j] * w2[i][j]
-
-        o1.append(tanh(total + b2[i]))
-
+    h1 = tanh(w1.dot(inputs[:, 0, None]) + b1)
+    h1 = h1.T[0]
+    o1 = tanh(h1.dot(w2) + b2)
     return o1
 
 # @param o1 = the returned output layer, a list of 10 floats
@@ -77,14 +62,21 @@ def cost(o1, correct):
             total += v ** 2 
     return total
 
-for i in range(3):
-    img = data[i]
-    print_image_and_label(img)
+if __name__ == '__main__':
+    data = pd.read_csv("data/train.csv")
+    data = np.array(data)
 
-    inputs = img[1:] # everything but the label
-    w1, b1, w2, b2 = init_weights_biases()
-    res = feed_forward(inputs, w1, b1, w2, b2)
-    res = list(res)
-    print(f"Prediction: { res.index(max(res)) }")
-    print(f"Cost: {cost(res, img[0])}")
-    print("————————————————————————————————————————————————————————\n")
+    for i in range(3):
+        img = data[i]
+        print_image_and_label(img)
+
+        inputs = np.array(img[1:]) # everything but the label
+        inputs = np.reshape(inputs, (784, 1)) 
+        inputs = inputs / 255 # put them on a scale of 1
+
+        w1, b1, w2, b2 = init_weights_biases()
+        res = feed_forward(inputs, w1, b1, w2, b2)
+        res = list(res[0])
+        print(f"Prediction: { res.index(max(res)) }")
+        print(f"Cost: {cost(res, img[0])}")
+        print("————————————————————————————————————————————————————————\n")
