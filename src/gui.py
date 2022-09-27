141fae0dda1a1cf940fa6614f9ce8e7ec054f2b2
Nick Viscomi <nicholasviscomi@gmail.com>
2022-09-27 08:32:46 -0400

    made new data file where the images are in binary

---
+++ src/gui.py
@@ -0,0 +1,120 @@
+from network import *
+import pygame
+from pygame.locals import *
+import sys
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+FPS = 60
+fps_clock = pygame.time.Clock()
+
+SCALE = 20
+WIDTH = 28 * SCALE
+HEIGHT = 28 * SCALE
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+
+cells = np.zeros((28, 28))
+
+def fill_nearby(mouse_x, mouse_y):
+    y = int((mouse_y)/SCALE)
+    x = int(mouse_x/SCALE)
+    cells[y][x] = 1
+
+    if x <= 1 or x >= 27:
+        return
+    if y <= 1 or y >= 27:
+        return
+
+    cells[int((mouse_y)/SCALE) + 1][int(mouse_x/SCALE)] = 1
+    cells[int((mouse_y)/SCALE) - 1][int(mouse_x/SCALE)] = 1
+
+    cells[int((mouse_y)/SCALE)][int(mouse_x/SCALE) + 1] = 1
+    cells[int((mouse_y)/SCALE)][int(mouse_x/SCALE) - 1] = 1
+
+def game_loop():
+    print("start game loop!")
+    dragging = False
+
+    name = "100hidden"
+    W1, B1, W2, B2 = np.load(f'models/{name}/master.npy', allow_pickle=True)
+    while True:
+        WINDOW.fill((255,0,0))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                dragging = True
+                mx, my = event.pos
+                fill_nearby(mx, my)        
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                dragging = False
+                mx, my = event.pos
+                fill_nearby(mx, my)
+            elif event.type == pygame.MOUSEMOTION:
+                if dragging:
+                    mx, my = event.pos
+                    fill_nearby(mx, my)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    # cells = np.zeros((28, 28)) something is buggy about this line of code
+                    print("clear!")
+                if event.key == pygame.K_RETURN:
+                    print("predict!")
+                    inputs = cells.flatten().reshape((784, 1))
+
+                    count = 0
+                    for pixel in cells.flatten():
+                        if pixel != 0:
+                            print("⬜️", end='')
+                        else:
+                            print("⬛️", end='')
+                            
+                        count += 1
+                        if count % 28 == 0: print("\n", end='') # add a new line
+
+                    prediction = make_predictions(inputs, W1, B1, W2, B2)
+                    print(f"Prediction: {prediction[0]}")
+                    for y, row in enumerate(cells):
+                        for x, val in enumerate(row):
+                            cells[y][x] = 0
+        
+        for y, row in enumerate(cells):
+            for x, val in enumerate(row):
+                col = BLACK
+                if val == 1: 
+                    col = WHITE
+                pygame.draw.rect(WINDOW, col, (x * SCALE, y * SCALE, SCALE, SCALE))      
+
+        pygame.display.update()
+        fps_clock.tick(FPS)
+
+if __name__ == '__main__':   
+    # name = "16hidden"
+    # W1, B1, W2, B2 = np.load(f'models/{name}/master.npy', allow_pickle=True)
+    # dev_predictions = make_predictions(X_dev, W1, B1, W2, B2)
+    # print(f"Test Accuracy: {get_accuracy(dev_predictions, Y_dev)}")
+    # game_loop()
+    print(raw_data.shape)
+
+    for y, row in enumerate(data):
+        for x, val in enumerate(row):
+            if x == 0: continue
+            if val != 0:
+                data[y][x] = 1
+
+    pd.DataFrame(data).to_csv('data/mod_train.csv')
+    
+    count = 0
+    for pixel in data[0][1:]:
+        if pixel == 1:
+            print("⬜️", end='')
+        else:
+            print("⬛️", end='')
+            
+        count += 1
+        if count % 28 == 0: print("\n", end='') # add a new line
---
+++ src/network.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+
+# Structure of data:
+# first column is the label of what the number actually is
+# All the other columns are the values for each pixel, either 0 or 1
+# Thus, one row is one imagez
+raw_data = pd.read_csv('data/train.csv')
+data = np.array(raw_data)
+m, n = data.shape # rows, cols
+np.random.shuffle(data) # shuffle before splitting into dev and training sets
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
+
+data_train = data[1000:m].T
+Y_train = data_train[0] # labels for all of the images
+X_train = data_train[1:n]
+X_train = X_train / 255.
+_,m_train = X_train.shape
+
+# 748 inputs, 16 hidden, 10 output
+W1 = [] # first weights = 748 * 16
+B1 = [] # first biases = 16 for hidden nodes
+
+W2 = [] # 16 * 10
+B2 = [] # second biases = 10 for output nodes
+
+def init_weights_biases(n_hidden):
+    W1 = np.random.rand(n_hidden, 784) - 0.5 # 16 arrays with 748 random numbers from -0.5 to 0.5
+    B1 = np.random.rand(n_hidden, 1) - 0.5
+    W2 = np.random.rand(10, n_hidden) - 0.5
+    B2 = np.random.rand(10, 1) - 0.5
+    return W1, B1, W2, B2
+
+def tanh(x):
+    return np.tanh(x)
+
+def deriv_tanh(x):
+    return 1 / (np.cosh(x) ** 2)
+
+def softmax(Z):
+    A = np.exp(Z) / sum(np.exp(Z))
+    return A
+
+def feed_forward(inputs, W1, B1, W2, B2):
+    Z1 = W1.dot(inputs) + B1 # (nh x 784) * (784 x 1) = 16 x 1
+    A1 = tanh(Z1) 
+    Z2 = W2.dot(A1) + B2 # (10 x nh) * (nh x 1) = 10 x 1
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def one_hot(Y):
+    # make an array that has 10 numbers. Everything is 0, 
+    # except for the position of the correct label, which is one
+
+    # When doing backpropagation, we want to turn off the bright neurons
+    # that were wrong and keep on the correct ones. Thus, when calculating what
+    # needs to change the most, the correct neuron needs to change by 0 and all
+    # the others need to change by their value so that they become 0. This means
+    # that the correct position of the correct label needs to be 1 so it doesn't
+    # change, and all the other ones do.
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    return one_hot_Y.T
+
+def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    # y = correct label of the image
+
+    # see how much the output layer, A2 needs to change compared to correct answer
+    dZ2 = A2 - one_hot(Y)
+    dW2 = dZ2.dot(A1.T) / m
+    db2 = np.sum(dZ2) / m
+
+    dZ1 = W2.T.dot(dZ2) * deriv_tanh(Z1)
+    dW1 = dZ1.dot(X.T) / m
+    db1 = np.sum(dZ1) / m
+    return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1    
+    W2 = W2 - alpha * dW2  
+    b2 = b2 - alpha * db2    
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    return np.sum(predictions == Y) / Y.size
+
+def gradient_descent(X, Y, learning_rate, iterations, n_hidden):
+    W1, b1, W2, b2 = init_weights_biases(n_hidden)
+
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = feed_forward(X, W1, b1, W2, b2)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, learning_rate)
+        if i % 10 == 0:
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    return W1, b1, W2, b2
+
+def make_predictions(inputs, W1, B1, W2, B2):
+    _, _, _, A2 = feed_forward(inputs, W1, B1, W2, B2)
+    predictions = get_predictions(A2)
+    return predictions
+
+def print_image_and_label(image, label, prediction):
+    print(f"Label: {label}")
+    print(f"Prediction: {prediction}")
+    
+    count = 0
+    for pixel in image.T.flatten():
+        if pixel != 0:
+            print("⬜️", end='')
+        else:
+            print("⬛️", end='')
+            
+        count += 1
+
+        if count % 28 == 0:
+            print("\n", end='') # add a new line
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print_image_and_label(current_image, label, prediction)
+
+def download_model(W1, B1, W2, B2, name):
+    master = np.array([W1, B1, W2, B2], dtype=object)
+    np.save(f"models/{name}/master.npy", master)
+    return
